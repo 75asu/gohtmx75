4267bb5a95517328c246e4db78aea00202fca303
75asu <crispyasutoshpanda3@gmail.com>
2024-03-05 12:50:24 +0530

    use hx-target with templates

--- cmd/main.go
+++ cmd/main.go
@@ -42,7 +42,7 @@ func main() {
 
 	e.POST("/count", func(c echo.Context) error {
 		count.Count++
-		return c.Render(200, "index", count)
+		return c.Render(200, "count", count)
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
