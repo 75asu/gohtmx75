33b6f55b6eca4b4f788eea21d61dda6e52f10557
75asu <crispyasutoshpanda3@gmail.com>
2024-03-05 15:46:51 +0530

    add error handling and change index

--- cmd/main.go
+++ cmd/main.go
@@ -41,6 +41,16 @@ type Data struct {
 	Contacts Contacts
 }
 
+// Go over the context which is "Data", if an email already exists then return true
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
 func newData() Data {
 	return Data{
 		Contacts: []Contact{
@@ -54,23 +64,62 @@ type Count struct {
 	Count int
 }
 
+// To return something more along with the existing form, prolly an error
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+// due to error handling, the index page is now messed up
+// so here goes a little index page
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	data := newData()
+	page := newPage()
 	e.Renderer = newTemplates()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", data)
+		return c.Render(200, "index", page)
 	})
 
 	e.POST("/contacts", func(c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
 
-		data.Contacts = append(data.Contacts, newContact(name, email))
-		return c.Render(200, "index", data)
+		if page.Data.hasEmail(email) {
+			// Do something with the errors
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			// Can't use 400 because here nothing is semantically wrong, it doesn't do a bad request
+			// the entity itself becomes unprocessable, so better to use 422
+			return c.Render(422, "form", formData)
+		}
+		page.Data.Contacts = append(page.Data.Contacts, newContact(name, email))
+
+		return c.Render(200, "display", page)
 	})
 	e.Logger.Fatal(e.Start(":8888"))
 }
